6a33bf2dde9f276a68552e51bf8e2045f12b4e0a
Truth <80925511+truth0829@users.noreply.github.com>
2021-08-05 20:31:08 +0900

    Update index.js

--- src/components/animate/index.js
+++ src/components/animate/index.js
@@ -1,5 +1,1 @@
-export * from './variants';
-export { default as MotionInView } from './MotionInView';
-export { default as MotionContainer } from './MotionContainer';
-export { default as DialogAnimate } from './DialogAnimate';
-export { default as ButtonAnimate } from './ButtonAnimate';
+
